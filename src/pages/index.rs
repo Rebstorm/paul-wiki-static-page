61d80063c7601bca53e615e2df3a57cb3e286823
Paul Jacks <Rebstorm@users.noreply.github.com>
2022-12-20 12:11:05 +0100

    index page

--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,4 +1,5 @@
 use yew::{function_component, html, Html};
+use yew_router::prelude::*;
 
 
 
@@ -6,11 +7,23 @@ use crate::components::{
     main_view::MainView
 };
 
+use crate::navigation::route::AvailableRoutes;
+
+
 #[function_component(Index)]
 pub fn index() -> Html {
     html! {
         <div class="index">
-          <MainView title="Paul" >{"Hello you fucks 123213"}</MainView>
+          <MainView title="Paul" >
+                <p>{"Welcome."} </p>
+                <p>{"Glad that you want to know more about me."}</p>
+                <p>{"I do dev."}</p>
+                <p>{"Sometimes I do real dev work as well. Would you like to know more? Check out my "}
+                    <Link<AvailableRoutes> classes="target" to={AvailableRoutes::Info}>
+                        {"current stack."}
+                    </Link<AvailableRoutes>>
+                </p>
+            </MainView>
         </div>
     }
 }
