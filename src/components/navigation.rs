2935f49135087257c4a5f2409eaa805cb86b2961
Paul Jacks <Rebstorm@users.noreply.github.com>
2022-12-20 10:45:07 +0100

    functional navigation I am ok with

--- src/components/navigation.rs
+++ src/components/navigation.rs
@@ -5,17 +5,16 @@ use crate::navigation::route::AvailableRoutes;
 
 #[function_component(Navigation)]
 pub fn navigation() -> Html {
-
     html! {
         <nav class="container">
-            <Link<AvailableRoutes> to={AvailableRoutes::Home}>
-                <div>{"home we go"} </div>
+            <Link<AvailableRoutes> classes="target" to={AvailableRoutes::Home}>
+                <div class="icon home" aria-describedby="home" />
             </Link<AvailableRoutes>>
-            <Link<AvailableRoutes> to={AvailableRoutes::Info}>
-                { "Info" }
+            <Link<AvailableRoutes> classes="target" to={AvailableRoutes::Info}>
+                <div class="icon syntax" aria-describedby="Pauls stack" />
             </Link<AvailableRoutes>>
-            <Link<AvailableRoutes> to={AvailableRoutes::NotFound}>
-                { "Copyright" }
+            <Link<AvailableRoutes> classes="target" to={AvailableRoutes::NotFound}>
+                <div class="icon copyright" aria-describedby="copyright" />
             </Link<AvailableRoutes>>
         </nav>
     }
