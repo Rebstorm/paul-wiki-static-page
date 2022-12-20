b4de5af67caad5341b77378a653cb16d8a2e4849
Paul Jacks <Rebstorm@users.noreply.github.com>
2022-12-20 12:49:34 +0100

    weird but working stack page

--- src/pages/info.rs
+++ src/pages/info.rs
@@ -8,8 +8,16 @@ use crate::components::{
 pub fn info() -> Html {
     html! {
         <MainView title="Stack">
-            <div> {"hi"} </div>
-            <div> {"bye"} </div>
+            <h2 id="projects-contributed-to">{"Projects contributed to"}</h2>
+            <p><a href="https://github.com/Rebstorm/yah">{"Yah (Yet another home screen)"}</a>{": A home screen with several integrations like SolarEdge, Roomba, Philips Hue and more. Written in TS."}</p>
+            <p><a href="https://github.com/denolib/awesome-deno">{"Deno"}</a>{": Collection of Deno list"}</p>
+            <p><a href="https://github.com/tw-in-js/twind">{"twind"}</a>{" Tailwind-In-JS"}</p>
+
+            <h2 id="current-stack">{"Current interest"}</h2>
+            <img src="static/ts.svg"/>{" "}
+            <img src="static/rust.png"/>
+            <br />
+            <h3 style="margin-bottom: auto" id="written in">{"This page was written in rust using WASM & "} <a href="https://yew.rs/">{"Yew"}</a></h3>
         </MainView>
     }
 }
